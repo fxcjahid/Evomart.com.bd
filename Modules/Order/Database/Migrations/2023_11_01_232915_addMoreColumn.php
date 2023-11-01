5f9458b25ddbdaedff7f48a8ff24c247e5504a6f
fxcjahid <fxcjahid3@gmail.com>
2023-11-01 23:49:03 +0600

    Changes to be committed: 	modified:   .timetracker 	new file:   Modules/Order/Database/Migrations/2023_11_01_232915_addMoreColumn.php

---
+++ Modules/Order/Database/Migrations/2023_11_01_232915_addMoreColumn.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMoreColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('customer_name')->nullable()->after('customer_phone');
+            $table->string('courier_name')->nullable()->after('shipping_country');
+            $table->string('create_by')->nullable()->after('shipping_country');
+            $table->string('creator_id')->nullable()->after('shipping_country');
+            $table->decimal('advance_pay', 18, 4)->unsigned()->after('discount');
+            $table->decimal('due_amount', 18, 4)->unsigned()->after('discount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('customer_name');
+            $table->dropColumn('courier_name');
+            $table->dropColumn('create_by');
+            $table->dropColumn('creator_id');
+            $table->dropColumn('advance_pay');
+            $table->dropColumn('due_amount');
+        });
+
+    }
+}
